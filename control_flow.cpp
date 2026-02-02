74b3be8f9d62a761b90b707b99725a114d0e3b85
Student-Prakul <prakul25.doe.svc.du@gmail.com>
2026-02-02 22:37:43 +0530

    initial commit

---
+++ control_flow.cpp
@@ -0,0 +1,26 @@
+// Q1.) Check for Even and Odd numbers
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "enter the value of num :- ";
+    cin >> num;
+    if(num%2==0){
+        cout << "the " << num << " is even" << "\n";
+        cout << "programme is executed";
+    }
+    else{
+        cout << "the " << num << " is odd" << "\n";
+        cout << "programme is executed";
+    }
+    return 0;
+}
+// Q2.) Largest among three
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1,num2,num3;
+    
+}
---
+++ index.cpp
@@ -0,0 +1,108 @@
+// // Question no. 1
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "hello world";
+// }
+// // Question no. 2
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int name;
+//     cout << "enter your name";
+//     cin >> name;
+//     cout << "hello" << name << "Good morning";
+//     return 0; 
+// }
+// // Question no. 3
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int num1,num2;
+//     cout << num1+num2;
+//     return 0;
+// }
+// // Question no. 4
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int num_swap,a,new_number;
+//     float b;
+//     cout << "enter the number";
+//     cin >> num_swap;
+//     a = num_swap%10;
+//     b = (float)num_swap/10;
+//     new_number = a*10+b;
+//     cout << "the new number is :- " << new_number;
+//     return 0;
+// }
+// // Question no. 5
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << sizeof(int) << "\n";
+//     cout << sizeof(char) << "\n";
+//     cout << sizeof(double) << "\n";
+//     cout << sizeof(float) << "\n";
+//     return 0;
+// }
+// // Question no. 6
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     float num1,num2,Multiplication;
+//     cout << "enter num1 :- ";
+//     cin >> num1;
+//     cout << "enter num2 :- ";
+//     cin >> num2;
+//     Multiplication = num1*num2;
+//     cout << Multiplication;
+//     return 0;
+// }
+// // // Question no. 7
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     char character;
+//     character = 'A';
+//     cout << "The ASCII value of " << character << " is " << int(character);
+//     return 0;
+// }
+// // Question no. 8
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int fahrenheit,celsius;
+//     cout << "enter the value of fahrenheit";
+//     cin >> fahrenheit;
+//     celsius = (fahrenheit - 32)*(float)5/9;
+//     cout << "fahrenheit to celsius " << celsius;
+//     return 0;
+// }
+// // Question no. 9
+// #include <iostream>
+// using namespace std;
+
+// void area_of_rectangle(int length,int breadth){
+//     int area = length * breadth;
+//     cout << "the area of the rectangle :- " << area;
+// }
+// void perimeter_of_rectangle(int length,int breadth){
+//     int perimeter = 2*(length+breadth);
+//     cout << "the perimeter is :- " << perimeter;
+// }
+// int main(){
+//     area_of_rectangle(2,3);
+//     cout << "\n";
+//     perimeter_of_rectangle(2,3);
+//     return 0;
+// }
+// // ------------------------------------------------------------------------------
