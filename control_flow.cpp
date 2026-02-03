294a5f4de9e93db10eaf16ee409cc23c95371d13
Student-Prakul <prakul25.doe.svc.du@gmail.com>
2026-02-03 20:08:30 +0530

    adding more questions

--- control_flow.cpp
+++ control_flow.cpp
@@ -1,26 +1,48 @@
-// Q1.) Check for Even and Odd numbers
+// // Q1.) Check for Even and Odd numbers
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int num;
+//     cout << "enter the value of num :- ";
+//     cin >> num;
+//     if(num%2==0){
+//         cout << "the " << num << " is even" << "\n";
+//         cout << "programme is executed";
+//     }
+//     else{
+//         cout << "the " << num << " is odd" << "\n";
+//         cout << "programme is executed";
+//     }
+//     return 0;
+// }
+// // Q2.) Largest among three
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n , max=0;
+//     cout << "enter the length of an array :- ";
+//     cin >> n;
+//     int num[n];
+//     for(int i=0;i<n;i++){
+//         cout << "enter the number :- ";
+//         cin >> num[i];
+//     }
+//     for(int i=0;i<n;i++){
+//         if(num[i]>max){
+//             max = num[i];
+//         } else {
+//             continue ;
+//         }
+//     }
+//     cout << "the maximumm number is :- " << max;
+//     return 0;
+// }
+// // Q3.) Vowel consonant check 
 #include <iostream>
 using namespace std;
 
 int main(){
-    int num;
-    cout << "enter the value of num :- ";
-    cin >> num;
-    if(num%2==0){
-        cout << "the " << num << " is even" << "\n";
-        cout << "programme is executed";
-    }
-    else{
-        cout << "the " << num << " is odd" << "\n";
-        cout << "programme is executed";
-    }
-    return 0;
-}
-// Q2.) Largest among three
-#include <iostream>
-using namespace std;
-
-int main(){
-    int num1,num2,num3;
     
 }
